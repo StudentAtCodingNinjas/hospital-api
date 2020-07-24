0ab6ed03249dc44efe49a043b70a2d374da3bb27
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-07-25 02:08:23 +0530

    Add files via upload

---
+++ models/doctor.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose');
+
+const doctorSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    patients:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Patient'
+    }]
+},{
+    timestamps:true
+});
+
+
+const Doctor=mongoose.model('Doctor',doctorSchema);
+module.exports=Doctor;
--- models/report.js
+++ models/report.js
@@ -1,1 +1,24 @@
+const mongoose=require('mongoose');
 
+
+const reportSchema=new mongoose.Schema({
+    patient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Patient'
+    },
+
+    status:{
+        type:String,
+        required:true,
+        enum:['Negative','Travelled-Quarantine','Symptoms-Quarantine','Positive-Admit']
+    },
+
+doctor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Doctor'
+    }
+},{
+    timestamps:true
+});
+const report=mongoose.model('Report',reportSchema);
+module.exports=report;
