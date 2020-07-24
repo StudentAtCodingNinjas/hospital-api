4fa963817c9bb9a8f2c47703efbf5827bb610f67
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-07-25 02:10:54 +0530

    Add files via upload

--- controllers/api/doctor_controller.js
+++ controllers/api/doctor_controller.js
@@ -1,1 +1,122 @@
+const Doctor=require('../models/doctor');
+const Patient=require('../models/patient');
+const Report=require('../models/report');
+const jwt=require('jsonwebtoken');
 
+module.exports.Register_Doctor=async (req,res)=>{
+    
+    if(!req.body.name || !req.body.email || !req.body.password){
+        return res.status(404).json({
+            message:"Fill all required fields"
+        })
+    }
+
+    try{
+        const doctor=new Doctor({
+
+            
+            email:req.body.email,
+            password:req.body.password,
+            name:req.body.name
+        });
+        
+        const newDoctor=await doctor.save();
+        res.status(201).json(newDoctor);
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
+
+
+module.exports.login=async (req,res)=>{
+    
+    if(!req.body.email || !req.body.password){
+        return res.status(404).json({
+            message: "required fields not filled"
+        });
+    }
+
+    try{
+
+        let doctor=await Doctor.findOne({email:req.body.email});
+        
+        if(!doctor||doctor.password!=req.body.password)
+        {
+            return res.status(422).json({
+                message:"Invalid username or password"
+            });
+        }
+        
+        return res.status(200).json({
+            message:'Signed in successfully',
+            data:{
+                
+                token:jwt.sign(doctor.toJSON(),'hospital_api',{expiresIn:'10000000'})
+            }
+        });
+    }
+    catch(err)
+    {
+        return res.json(500,{
+            message:'Internal Server error'
+        });
+    }
+}
+
+
+module.exports.Register_Patient=async (req,res)=>{
+    try{
+        let doctor=await Doctor.findOne({_id:req.user});
+        
+        
+        if(!doctor)
+        {
+            throw new Error('Doctor not found in the database');
+        }
+        let patient=await Patient.findOne({phone:req.body.phone});
+        if(patient)
+        {
+            return res.status(201).json({
+                message:"Patient Already exists",
+                patient
+            })
+        }
+        const newPatient=await Patient.create({
+            phone:req.body.phone,
+            name:req.body.name,
+            doctor:req.user,
+            report:req.body.report
+        });
+        doctor.patient.push(newPatient);
+        doctor.save();
+        return res.status(201).json({
+            message:"new patient added",
+            newPatient
+        })
+
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}  
+
+
+module.exports.status=async (req,res)=>{
+    try{
+        let report=await Report.findOne({status:req.params.status});
+        if(report)
+        {
+            return res.status(201).json({
+                message:"reports",
+                report
+            })
+        }
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
---
+++ controllers/api/patient_controller.js
@@ -0,0 +1,73 @@
+const Report=require('../models/report');
+const Doctor=require('../models/doctor');
+const Patient=require('../models/patient');
+
+
+module.exports.createReport=async (req,res)=>{
+    try{
+
+    
+        let doctor=await Doctor.findOne({_id:req.user});
+        let patient=await Patient.findById(req.params.id);
+        if(patient)
+        {
+            const newreport=await Report.create({
+                status:req.body.status,
+                patient:req.params.id,
+                doctor:req.user,
+                date:req.body.date
+            });
+            patient.report.push(newreport);
+            patient.save();
+            res.status(201).json(newreport);
+        }
+        else{
+            res.status(401).json({
+                message:"Patient not found in the database"
+            })
+        }
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+} 
+
+// all the reports
+module.exports.get_All_Reports=async function(req,res)
+{
+    try {
+        let patient=await Patient.findOne({_id:req.params.id})
+        .populate({path: 'doctor', select: ['name','email']})
+        .populate({path: 'report', options:{sort: {createdAt:1}}})
+
+        return res.status(200).json({
+            message:"Reports",
+            patient
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
+
+// status of report
+module.exports.status=async function(req,res)
+{
+    try{
+        let report=await Report.find({status:req.params.status});
+        if(report)
+        {
+            return res.status(201).json({
+                message:"reports",
+                report
+            })
+        }
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
